323b21c46f78ddb8fe75834c54dab8643430a461
elliot <cuskellyn@gmail.com>
2024-09-08 23:38:33 +1000

    update

--- frontend/src/components/NewContact.js
+++ frontend/src/components/NewContact.js
@@ -26,12 +26,20 @@ function NewContact(props) {
         setName('');
     }
 
-	return (
+
+    return (
         <form className='new-contact' onSubmit={createContact}>
-            <input type='text' placeholder='Name' onChange={(e) => setName(e.target.value)} value={name}/>
+            <select onChange={(e) => setName(e.target.value)} value={name}>
+                <option value="" disabled selected>Select category</option>
+                <option value="Family">Family</option>
+                <option value="Friend">Friend</option>
+                <option value="Work">Work</option>
+                <option value="Other">Other</option>
+            </select>
             <button className='button green' type='submit'>Create Contact</button>
         </form>
-	);
+    );
+
 }
 
 export default NewContact;
